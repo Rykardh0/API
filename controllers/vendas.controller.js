515f9e1a3b475af3b1bd52a974f1513aa292f0c1
Rykardh0 <hssricardo@gmail.com>
2025-11-07 20:43:52 -0300

    "Aula_07.11"

--- controllers/vendas.controller.js
+++ controllers/vendas.controller.js
@@ -5,4 +5,19 @@ module.exports = () => {
     controller.listaVendas = (req, res) => res.status(200).json(dataDB);
 
     return controller;
+
+    controller.salvarVendas = (req, res) =>{
+        dataDB.customerWallets.data.push({
+            id: uuid,
+            parentId: uuid,
+            name: req.body.name,
+            birthDate: req.body.birthDate,
+            cellphone: req.body.cellphone,
+            phone: req.body.phone,
+            email: req.body.email,
+            occupation: req.body.occupation,
+            state: req.body.state,
+        });
+        res.status(201).json(db);
+    }
 }
